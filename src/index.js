c7b24c045fc17b395a2d1661dfbc17bb49a3b414
Jisse Reitsma <jisse@yireo.com>
2019-07-28 10:42:07 +0200

    Pending changes

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import './index.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import itemApp from './reducers'
-import { createStore } from 'redux'
-import { loadState, saveState } from './localStorage'
-import throttle from 'lodash/throttle'
-
-const persistedState = loadState()
-const store = createStore(itemApp, persistedState)
-
-store.subscribe(throttle(() => {
-  saveState({
-    itemsReducer: {
-      items: store.getState().itemsReducer.items
-    }
-  });
-}, 1000));
+import * as serviceWorker from './serviceWorker';
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -26,4 +13,4 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'));
 
-//serviceWorker.unregister();
+serviceWorker.register();
